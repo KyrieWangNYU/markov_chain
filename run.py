6590e043551d2bb7d0471043c2ab723df6f28d2e
KyrieWangNYU <kw2116@nyu.edu>
2017-09-06 17:00:28 -0400

    first success

--- run.py
+++ run.py
@@ -2,10 +2,10 @@ from markov_python.cc_markov import MarkovChain
 
 mc = MarkovChain()
 mc.add_file("content.txt")
-# with open("content.txt") as f:
-#     content = f.readlines()
-# content = [x.strip() for x in content]
-res = mc.generate_text(5)
+length = raw_input("Enter the number of word you would like to generate: ")
+con = []
+while len(con) < int(length):
+    con += mc.generate_text(20)
+res = " ".join(con)
 with open("res.txt", "r+") as f:
     f.writelines(res)
-
