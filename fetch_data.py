25fd484d22f545ac4d048809b7d49fab530f488d
KyrieWangNYU <kw2116@nyu.edu>
2017-09-05 23:36:28 -0400

    update

--- fetch_data.py
+++ fetch_data.py
@@ -3,18 +3,28 @@ from markov_python.cc_markov import MarkovChain
 from bs4 import BeautifulSoup
 
 # url = raw_input("Enter the address you want to retrieve: ")
-url = 'http://www.google.com'
+url = 'https://ebooks.adelaide.edu.au/c/chesterton/gk/c52fb/chapter1.html'
 request = urllib2.Request(url)
 response = urllib2.urlopen(request)
 page = response.read()
 
-# with open("page.html", "r+") as f:
-#     f.write(page)
+with open("page.html", "r+") as f:
+    f.write(page)
 
-# with open("page.html") as fp:
+# with open("page.txt") as fp:
 #     soup = BeautifulSoup(fp)
 
 soup = BeautifulSoup(str(page),"html.parser")
+content = soup.find_all('p')
+# print content
+if len(content) > 0:
+    for i in content:
+        with open("content.txt", "r+") as f:
+            f.writelines(str(i))
 
-
-print soup.prettify()
+mc = MarkovChain()
+mc.add_file("content.txt")
+res = mc.generate_text(20)
+with open("res.txt", "r+") as f:
+    f.writelines(res)
+# print soup.prettify()
