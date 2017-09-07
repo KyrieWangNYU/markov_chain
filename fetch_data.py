e34da2190ffde774b50cfc729a3784faa67bcda0
KyrieWangNYU <kw2116@nyu.edu>
2017-09-07 16:20:37 -0400

    Final Result

--- fetch_data.py
+++ fetch_data.py
@@ -13,17 +13,40 @@ class fetch_data:
         request = urllib2.Request(url)
         response = urllib2.urlopen(request)
         page = response.read()
-        # with open("page.html", "r+") as f:
-        #     f.write(page)
-        # f = open("page.html", "r+")
-        # return f
-        return page
+        with open("page.html", "r+") as f:
+            f.write(page)
+        f = open("page.html", "r+")
+        return f
 
-    def extract_conent(self, page):
-        soup = BeautifulSoup(page, "html.parser")#only "html parser" works on my mac
-        content = ''
+
+    def extract_conent(self, f):
+        soup = BeautifulSoup(f, "html.parser")#only "html parser" works on my mac
+        content = []
         for i in soup.find_all('p'):
-            content += soup.find_all('p')[i].text
+            content.append(i.text)
+        content = "".join(content)
         content = content.encode('ascii', 'ignore').decode('ascii')
         with open("content.txt", "r+") as f:
             f.write(content)
+
+
+
+
+# with open("page.txt") as fp:
+#     soup = BeautifulSoup(fp)
+# f = open("page.html", "r+")
+# soup = BeautifulSoup(f, "html.parser")#only "html parser" works on my mac
+# content = soup.find_all('p').text
+# content = content.encode('ascii', 'ignore').decode('ascii')
+# with open("content.txt", "r+") as f:
+#     f.write(content)
+# print type(soup)
+#print soup.text
+# content = soup.find_all('p')
+# # print content
+#
+
+# content = soup.find_all('p')[0].text
+# content = content.encode('ascii', 'ignore').decode('ascii')
+# with open("content.txt", "r+") as f:
+#     f.write(content)
--- run.py
+++ run.py
@@ -1,28 +1,29 @@
 from markov_python.cc_markov import MarkovChain
 from fetch_data import *
+
+# Welcome
 welcome = "Welcome to markov_chain txt generater, you could choose " \
           "a website with novel or articles(e.g.online reader)"
 print welcome
+
+# choose website and fetch data
 data = fetch_data()
 url = raw_input("Enter the address you want to retrieve: ")
 fname = data.generate_file(url)
 data.extract_conent(fname)
+
+#generate words with markov_chain
 mc = MarkovChain()
 mc.add_file("content.txt")
 length = raw_input("Enter the number of words you would like to generate: ")
 con = []
 while len(con) < int(length):
-    con += mc.generate_text(20)
-res = " ".join(con)
+    con.append(mc.generate_text(20))
+res = ""
+for item in con:
+    item = " ".join(item)
+    res += item
+
 with open("result.txt", "r+") as f:
     f.writelines(res)
 print "Result has been stored in result.txt"
-# mc = MarkovChain()
-# mc.add_file("content.txt")
-# length = raw_input("Enter the number of word you would like to generate: ")
-# con = []
-# while len(con) < int(length):
-#     con += mc.generate_text(20)
-# res = " ".join(con)
-# with open("res.txt", "r+") as f:
-#     f.writelines(res)
